0584f7d9bee0b404dba1e833d1069426b7929b90
Kev Morel <kev.morel@northcoders.com>
2022-03-18 20:33:57 +0000

    pass test ERR: invalid path status 404: path not found

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { getStaff } = require('./controllers/staff.controllers');
+const { invalidPath } = require('./errors/errors');
 
 const app = express();
 
@@ -8,6 +9,6 @@ app.use(express.json());
 
 app.get('/api/staff', getStaff);
 
-
+app.all('/*', invalidPath);
 
 module.exports = app;
